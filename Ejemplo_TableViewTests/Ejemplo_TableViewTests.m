02f113041da7912d3196f3ca069c7f06baf42262
Leandro Fantin <leandro.fantin@mercadolibre.com>
2014-07-24 12:14:37 -0300

    Initial Commit

---
+++ Ejemplo_TableViewTests/Ejemplo_TableViewTests.m
@@ -0,0 +1,34 @@
+//
+//  Ejemplo_TableViewTests.m
+//  Ejemplo_TableViewTests
+//
+//  Created by Leandro Fantin on 24/07/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ejemplo_TableViewTests : XCTestCase
+
+@end
+
+@implementation Ejemplo_TableViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
